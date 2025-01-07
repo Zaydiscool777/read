8c07e8e4bb756cf580ed3feb91b16aea6398cb57
zaydm <Zaydiscool777@gmail.com>
2025-01-06 21:47:17 -0500

    Update lib.h

--- lib.h
+++ lib.h
@@ -3,6 +3,16 @@
 #include <memory.h>
 // to include: #include "lib.h"
 
+/*** DEBUG ***/
+
+int pass(int in)
+{
+    printf("\npass: %i %x\n", in, in);
+    return in;
+}
+
+/*** DATASTRUCTS ***/
+
 //http://www.cse.yorku.ca/~oz/hash.html
 //see also: https://gist.github.com/hmic/1676398
 unsigned long hash(unsigned char *str){
@@ -16,3 +26,26 @@ unsigned long hash(unsigned char *str){
 // sha256: use https://github.com/B-Con/crypto-algorithms/blob/master/sha256.c instead.
 
 // credit: https://www.geeksforgeeks.org/structures-c/
+
+/* DATASTRUCT linked list*/
+
+struct ll
+{
+    int datum;
+    struct ll* next;
+};
+
+int ll_s(struct ll self, int set)
+{
+    self.datum = set;
+}
+
+void ll_p(struct ll self, struct ll to)
+{
+    self.next = &to;
+}
+
+int ll_g(struct ll self)
+{
+    return self.datum;
+}
